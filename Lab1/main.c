21a0731b52640ab58eebb440f7ea8e43c03a655a
Kae-Young <kaeyoung1@outlook.com>
2024-08-08 22:50:48 +1000

    test adding a new file to github repository

---
+++ Lab1/main.c
@@ -0,0 +1,16 @@
+/**************************************************************
+ * main.c
+ * rev 1.0 08-Aug-2024 Kae Young
+ * Lab1
+ * ***********************************************************/
+
+#include "stdlib.h"
+#include <stdbool.h>
+
+
+int main(void) {
+  // TODO - Initialise components and variables
+  while (true) {
+    // TODO - Repeated code here
+  }
+}
