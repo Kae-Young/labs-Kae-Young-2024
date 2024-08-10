6134c53fab7dbb46ac865f36b2629b311d1ddc21
Kae-Young <kaeyoung1@outlook.com>
2024-08-10 21:28:29 +1000

    correct LED GPIO pin numbers

--- Lab3/main.c
+++ Lab3/main.c
@@ -12,24 +12,24 @@
 int main(void) {
   stdio_usb_init();
 
-  #define RED_LED 15
-  #define GREEN_LED 16
-  #define BLUE_LED 17
+  #define RED_LED 11
+  #define GREEN_LED 12
+  #define BLUE_LED 13
   
   gpio_init(RED_LED);
   gpio_init(GREEN_LED);
   gpio_init(BLUE_LED);
 
-  gpio_set_dir(RED_LED, 1);
-  gpio_set_dir(GREEN_LED, 1);
-  gpio_set_dir(BLUE_LED, 1);
+  gpio_set_dir(RED_LED, GPIO_OUT);
+  gpio_set_dir(GREEN_LED, GPIO_OUT);
+  gpio_set_dir(BLUE_LED, GPIO_OUT);
 
   while (true) {
     printf("On\r\n");
     gpio_put(RED_LED, 1);
-    sleep_ms(500);
+    sleep_ms(1000);
     printf("Off\r\n");
     gpio_put(RED_LED, 0);
-    sleep_ms(500);
+    sleep_ms(1000);
   }
 }
