dd1c2c40c20d8de4fe63aac756dd8ab85b0ac2ed
Kae-Young <kaeyoung1@outlook.com>
2024-08-10 21:06:10 +1000

    configure LED GPIOs

--- Lab3/main.c
+++ Lab3/main.c
@@ -4,13 +4,32 @@
  * Lab3
  * ***********************************************************/
 
-#include "stdlib.h"
+#include "pico/stdlib.h"
 #include <stdbool.h>
 #include <stdio.h>
 
 
 int main(void) {
+  stdio_usb_init();
+
+  #define RED_LED 15
+  #define GREEN_LED 16
+  #define BLUE_LED 17
+  
+  gpio_init(RED_LED);
+  gpio_init(GREEN_LED);
+  gpio_init(BLUE_LED);
+
+  gpio_set_dir(RED_LED, 1);
+  gpio_set_dir(GREEN_LED, 1);
+  gpio_set_dir(BLUE_LED, 1);
+
   while (true) {
-    printf("Hello world\r\n");
+    printf("On\r\n");
+    gpio_put(RED_LED, 1);
+    sleep_ms(500);
+    printf("Off\r\n");
+    gpio_put(RED_LED, 0);
+    sleep_ms(500);
   }
 }
