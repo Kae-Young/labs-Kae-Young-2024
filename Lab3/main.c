6af339001dd640f7f53928463016deddc00f5ff4
Kae-Young <kaeyoung1@outlook.com>
2024-08-10 13:28:41 +1000

    correct lab number

---
+++ Lab3/main.c
@@ -0,0 +1,16 @@
+/**************************************************************
+ * main.c
+ * rev 1.0 08-Aug-2024 Kae Young
+ * Lab3
+ * ***********************************************************/
+
+#include "stdlib.h"
+#include <stdbool.h>
+#include <stdio.h>
+
+
+int main(void) {
+  while (true) {
+    printf("Hello world\r\n");
+  }
+}
