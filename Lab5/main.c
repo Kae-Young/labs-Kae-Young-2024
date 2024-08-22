53166618a02b7dc1e0db570e00cf89fb7205f566
Kae-Young <kaeyoung1@outlook.com>
2024-08-22 16:39:52 +1000

    add stdio library and declare registers

--- Lab5/main.c
+++ Lab5/main.c
@@ -7,6 +7,13 @@
 #include "pico/stdlib.h"
 #include <stdbool.h>
 #include "hardware/pwm.h"
+#include <stdio.h>
+
+#define CONTENT_OF(addr)        (*(volatile uint32_t*)addr)
+#define PAD_CONTROL_BASE        0x4001c000
+#define REG_PAD_CONTROL_GPIO15  (PAD_CONTROL_BASE+0x40)
+#define CTRL_PDE                0x4
+#define CTRL_PUE                0x8
 
 int main(void) {
   // TODO - Initialise components and variables
