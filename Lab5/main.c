0457ba0568982be001ff643112f0485a59f74262
Kae-Young <kaeyoung1@outlook.com>
2024-08-22 16:19:23 +1000

    add lab5

---
+++ Lab5/main.c
@@ -0,0 +1,16 @@
+/**************************************************************
+ * main.c
+ * rev 1.0 22-Aug-2024 kaeyo
+ * Lab5
+ * ***********************************************************/
+
+#include "pico/stdlib.h"
+#include <stdbool.h>
+
+
+int main(void) {
+  // TODO - Initialise components and variables
+  while (true) {
+    // TODO - Repeated code here
+  }
+}
