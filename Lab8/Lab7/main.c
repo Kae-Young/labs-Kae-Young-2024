2be1cfcb34e77aa26cafca7e15f9b6a4ec847bc0
kae-young <kaeyoung1@outlook.com>
2024-09-18 19:21:23 +1000

    add week 7 lab

---
+++ Lab8/Lab7/main.c
@@ -0,0 +1,16 @@
+/**************************************************************
+ * main.c
+ * rev 1.0 18-Sep-2024 Kae Young
+ * Lab7
+ * ***********************************************************/
+
+#include "pico/stdlib.h"
+#include <stdbool.h>
+
+
+int main(void) {
+  // TODO - Initialise components and variables
+  while (true) {
+    // TODO - Repeated code here
+  }
+}
