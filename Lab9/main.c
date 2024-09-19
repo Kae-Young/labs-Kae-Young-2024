8976c720917132ce03c3fd195e786d29a072bde4
Kae-Young <kaeyoung1@outlook.com>
2024-09-19 16:35:17 +1000

    create transmitter code

--- Lab9/main.c
+++ Lab9/main.c
@@ -7,10 +7,67 @@
 #include "pico/stdlib.h"
 #include <stdbool.h>
 
+#define BLUE_LED    13
+#define RED_LED     12
+#define GREEN_LED   11
+#define SEND_BTN    2
+
+#define t_interval  10
 
 int main(void) {
-  // TODO - Initialise components and variables
+  //init RGB LED's
+  gpio_init(BLUE_LED);
+  gpio_set_dir(BLUE_LED, GPIO_OUT);
+  gpio_init(RED_LED);
+  gpio_set_dir(RED_LED, GPIO_OUT);
+  gpio_init(GREEN_LED);
+  gpio_set_dir(GREEN_LED, GPIO_OUT);
+
+  //init send button
+  gpio_init(SEND_BTN);
+  gpio_set_dir(SEND_BTN, GPIO_IN);
+
+  int n = 10;
+  bool signal[n];
+  signal = [1,0,1,0,1,1,0,0,1,0];
+
   while (true) {
-    // TODO - Repeated code here
+    if (gpio_get(SEND_BTN)) {
+      send_signal(signal, n);
+    }
   }
+}
+
+void send_signal(bool *signal, int n)  {
+  for (int i = 0; i < n; i++)
+  {
+    if (signal[i])
+    {
+      signal_1();
+    }
+    else
+    {
+      signal_0();
+    }
+    
+  }
+  
+}
+
+//light on
+void signal_1(void)  {
+  gpio_put(BLUE_LED, true);
+  gpio_put(GREEN_LED, true);
+  gpio_put(RED_LED, true);
+
+  sleep_ms(t_interval);
+}
+
+//light off
+void signal_0(void) {
+  gpio_put(BLUE_LED, false);
+  gpio_put(GREEN_LED, false);
+  gpio_put(RED_LED, false);
+
+  sleep_ms(t_interval);
 }
